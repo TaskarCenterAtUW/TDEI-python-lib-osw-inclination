7045bcb6dd93978c4af3ec44356dde71379862aa
sujata-m <sujatam@gaussiansolutions.com>
2024-09-17 22:41:02 +0530

    Updated package name

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(
-    name='osm-incline',
+    name='osw-incline',
     version=version,
     author='Sujata Misra',
     author_email='sujatam@gaussiansolutions.com',
